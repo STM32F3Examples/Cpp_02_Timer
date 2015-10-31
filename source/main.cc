6bc6d0df37feeebfd0fd8d7344c24329d79bb868
Manuel Limones <limonesu.me@gmail.com>
2015-10-31 05:43:35 -0600

    Change main to unix style

--- source/main.cc
+++ source/main.cc
@@ -19,7 +19,7 @@ int main(){
 void led_init(void){
 	//Turn on the GPIOB peripherial
 	RCC->AHBENR |= RCC_AHBENR_GPIOBEN;
-	//Configure PB13  as push pull ouput an set the output to high 
+	//Configure PB13  as push pull ouput an set the output to high
 	GPIOB->MODER &=~(GPIO_MODER_MODER13);
 	GPIOB->MODER |=GPIO_MODER_MODER13_0;//output
 	GPIOB->ODR |= GPIO_ODR_13;
